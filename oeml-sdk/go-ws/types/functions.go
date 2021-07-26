d3d74b537610efb27ed6d1a92e4e5276cb60edd7
Marvin <marvin.hansen@gmail.com>
2021-07-26 12:28:07 +0800

    Updated comments & formatting

--- oeml-sdk/go-ws/types/functions.go
+++ oeml-sdk/go-ws/types/functions.go
@@ -1,5 +1,3 @@
-// Copyright (c) 2021. Marvin Friedrich Lars Hansen. All Rights Reserved. Contact: marvin.hansen@gmail.com
-
 package types
 
 type UpdateInvoke struct {
--- oeml-sdk/go-ws/v1/set_methods.go
+++ oeml-sdk/go-ws/v1/set_methods.go
@@ -1,5 +1,3 @@
-// Copyright (c) 2021. Marvin Friedrich Lars Hansen. All Rights Reserved. Contact: marvin.hansen@gmail.com
-
 package v1
 
 import "go-ws/types"
